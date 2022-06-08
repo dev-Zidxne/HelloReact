1dd4730b0970aa760ba4e47891f6515b9ac86e3e
Zidane Innis <zidaneinnis@gmail.com>
2022-06-07 21:03:36 -0500

    O

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App(props) {
 
   return (
     <div>
-      <div className="todoapp stack-large">
+      <div className="todoapp stack-lare">
         <h1>TodoMatic</h1>
         <Form addTask={addTask} />
         <FilterButton />
