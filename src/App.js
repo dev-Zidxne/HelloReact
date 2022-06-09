60e0898935e2db95d0af1ea21fdd80baa3c5d77a
Zidane Innis <zidaneinnis@gmail.com>
2022-06-09 06:53:18 -0500

    COmpleted To Do App to full functionality.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import FilterButton from "./components/FilterButton.js";
 import React, { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const addTask = (name) => {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   };
+  const [filter, setFilter] = useState("All");
 
   const toggleTaskCompleted = (id) => {
     const updatedTasks = tasks.map((task) => {
@@ -34,15 +43,26 @@ function App(props) {
     });
     setTasks(editedTaskList);
   };
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -54,7 +74,7 @@ function App(props) {
       <div className="todoapp stack-lare">
         <h1>TodoMatic</h1>
         <Form addTask={addTask} />
-        <FilterButton />
+        {filterList}
         <h2 id="list-heading">{headingText}</h2>
         <ul
           role="list"
